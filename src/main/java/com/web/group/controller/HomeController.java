085e2ff3150b28be6d2875cd08ee271762d3694e
cjh3576 <48780830+cjh3576@users.noreply.github.com>
2020-07-03 19:42:37 +0900

    vue추가0703
    vue src안에 넣음

--- src/main/java/com/web/group/controller/HomeController.java
+++ src/main/java/com/web/group/controller/HomeController.java
@@ -1,11 +1,17 @@
 package com.web.group.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
+	
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "hello";
+	}
 	
 	@RequestMapping("/")
 	public ModelAndView home() throws Exception{
